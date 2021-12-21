24844d6d6371e3f3d9644732768ead4b81513a48
Allyssa <a.alimoestar@gmail.com>
2021-12-21 14:42:19 +0100

    :construction: burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var navLinks = document.getElementById("js-nav-links");
+var navToggle = document.getElementById("btn-toggle");
+var burger = document.querySelector(".btn-burger-toggle")
+
+navToggle.addEventListener("click", function () {
+    
+  navLinks.classList.toggle("active");
+  burger.classList.toggle("toggle");
+});
